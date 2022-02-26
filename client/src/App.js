36e97b58fe151a868f2e0e553b2b2e99c67a7c1c
Yeonju Park <yeonjupark95@gmail.com>
2022-02-26 12:42:13 -0800

    initial react setup

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import {Routes, Route } from 'react-router-dom';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Routes>
+          <Route path="/" />
+        </Routes>
+      </header>
+    </div>
+  );
+}
+
+export default App;
