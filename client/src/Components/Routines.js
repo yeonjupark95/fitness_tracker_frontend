721b44a18aa6a31e32591bcc20e9608f5a91b141
Yeonju Park <yeonjupark95@gmail.com>
2022-02-28 23:13:09 -0800

    Got routines to show everything including activities

--- client/src/Components/Routines.js
+++ client/src/Components/Routines.js
@@ -22,16 +22,31 @@ const Routines = (token) => {
       <h2>Routines</h2>
       {routines.length ? (
         routines.length > 0 &&
-        routines.map(({ id, creatorId, name, goal, creatorName, activities }) => {
-          return (
-            <div className="routines" key={id}>
-              <div className="routines-name">{name}</div>
-              <div className="routines-creator-name">{creatorName}</div>
-              <div className="routines-goal">{goal}</div>
-              <div className="activities"></div>
-            </div>
-          );
-        })
+        routines.map(
+          ({
+            id,
+            name,
+            goal,
+            creatorName,
+            activities: [
+              { name: activitiesName, description, duration, count },
+            ],
+          }) => {
+            return (
+              <div className="routines" key={id}>
+                <div className="routines-name">{name}</div>
+                <div className="routines-creator-name">{creatorName}</div>
+                <div className="routines-goal">{goal}</div>
+                <div className="routines-activities">
+                  <div className="activities-id">{activitiesName}</div>
+                  <div className="activities-name">{description}</div>
+                  <div className="activities-description">{duration}</div>
+                  <div className="activities-count">{count}</div>
+                </div>
+              </div>
+            );
+          }
+        )
       ) : (
         <h5> There are no routines to display! </h5>
       )}
