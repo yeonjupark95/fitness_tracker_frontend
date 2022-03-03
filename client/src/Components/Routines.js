5e5f7d62b5c402aba16ed8f05277419f1476e960
Yeonju Park <yeonjupark95@gmail.com>
2022-03-02 22:42:19 -0800

    got a new routine to show in routines

--- client/src/Components/Routines.js
+++ client/src/Components/Routines.js
@@ -1,11 +1,13 @@
 import { fetchRoutines } from "../api";
 import { useState, useEffect } from "react";
 
-const Routines = (token, routines, setRoutines) => {
+const Routines = (token) => {
+  const [routines, setRoutines] = useState([]);
   const handleRoutines = async () => {
     try {
       const newRoutines = await fetchRoutines();
       setRoutines(newRoutines);
+      console.log("routines", routines);
     } catch (error) {
       console.error(error);
     }
@@ -21,16 +23,7 @@ const Routines = (token, routines, setRoutines) => {
       {routines.length ? (
         routines.length > 0 &&
         routines.map(
-          ({
-            id,
-            isPublic,
-            name,
-            goal,
-            creatorName,
-            activities: [
-              { name: activitiesName, description, duration, count },
-            ],
-          }) => {
+          ({ id, isPublic, name, goal, creatorName, activities }) => {
             return (
               <>
                 {isPublic && (
@@ -39,15 +32,22 @@ const Routines = (token, routines, setRoutines) => {
                     <div className="routines-creator-name">{creatorName}</div>
                     <div className="routines-goal">{goal}</div>
                     <div className="routines-activities">
-                      <div className="activities-id">{activitiesName}</div>
-                      <div className="activities-name">{description}</div>
+                      {/* <div className="activities-name">{activities}</div>
                       <div className="activities-description">
-                        Duration: {duration}
+                        {activities.description}
                       </div>
-                      <div className="activities-count">Count: {count}</div>
+                      <div className="activities-duration">
+                        Duration: {activities.duration}
+                      </div>
+                      <div className="activities-count">
+                        Count: {activities.count}
+                      </div> */}
                     </div>
                   </div>
                 )}
+                {/* {isPublic && activities.length>0 (
+                  
+                )} */}
               </>
             );
           }
