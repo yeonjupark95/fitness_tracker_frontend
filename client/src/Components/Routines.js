da79fc3359f9f16738609eadcccc9bc7784e7165
Yeonju Park <yeonjupark95@gmail.com>
2022-03-04 22:53:28 -0800

    added singleRoutine

--- client/src/Components/Routines.js
+++ client/src/Components/Routines.js
@@ -1,5 +1,6 @@
 import { fetchRoutines } from "../api";
 import { useState, useEffect } from "react";
+import SingleRoutine from "./SingleRoutine";
 
 const Routines = (token) => {
   const [routines, setRoutines] = useState([]);
@@ -19,44 +20,7 @@ const Routines = (token) => {
 
   return (
     <div className="routines-wrapper">
-      <h2>Routines</h2>
-      {routines.length ? (
-        routines.length > 0 &&
-        routines.map(
-          ({ id, isPublic, name, goal, creatorName, activities }) => {
-            return (
-              <>
-                {isPublic && (
-                  <div className="routines" key={id}>
-                    <div className="routines-name">{name}</div>
-                    <div className="routines-creator-name">Creator: {creatorName}</div>
-                    <div className="routines-goal">{goal}</div>
-                    <h5> Activities: </h5>
-                  </div>
-                )}
-                {activities.length
-                  ? (activities.length > 0 && 
-                    activities.map((activity) => {
-                      const { id, name, description, duration, count } = activity;
-                      return(
-                        <>
-                          <div className="routine-activities" key={id}>
-                          <div className="routine-activities-name">Name: {name} </div>
-                          <div className="routine-activities-description">Description: {description} </div>
-                          <div className="routine-activities-duration">Duration: {duration} </div>
-                          <div className="routine-activities-count">Count: {count} </div>
-                          </div>
-                        </>
-                      )
-                    }))
-                  : (<div> There are no activities for this routine. </div>)}
-              </>
-            );
-          }
-        )
-      ) : (
-        <h5> There are no routines to display. </h5>
-      )}
+      <SingleRoutine routines={routines}/>
     </div>
   );
 };
